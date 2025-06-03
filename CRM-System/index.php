34422324b7bcfe321eb4c739ed7292a3e1ce0f76
duynghiadev <duynghia22302@gmail.com>
2025-06-03 15:45:28 +0700

    feat: add new variable and array operations to index.php

--- CRM-System/index.php
+++ CRM-System/index.php
@@ -5,6 +5,7 @@ $sum = 0;
 $count = 0;
 $max = $arr[0];
 $min = $arr[0];
+$old = 0;
 
 for ($i = 0; $i < count($arr); $i++) {
   $sum += $arr[$i];
@@ -16,6 +17,25 @@ for ($i = 0; $i < count($arr); $i++) {
     $min = $arr[$i];
   }
 }
+
+if ($old == 0) {
+  echo "old = 0 <br>";
+  echo "================ <br>";
+}
+
+define("OLD", 1);
+echo "old = " . OLD . "<br>";
+echo "================ <br>";
+
+$array1 = [1, 2, 3, 4, 5];
+$array2 = [6, 7, 8, 9, 10];
+$array3 = array_merge($array1, $array2);
+foreach ($array3 as $value) {
+  echo $value . " ";
+}
+echo "<br>";
+echo "================ <br>";
+
 
 echo "sum = $sum <br>";
 echo "count = $count <br>";
