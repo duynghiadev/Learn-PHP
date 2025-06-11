da4d7f08af8537537b922d712b56a6ca7865d9eb
duynghiadev <duynghia22302@gmail.com>
2025-06-11 16:20:54 +0700

    refactor: uncomment code to improve variable demonstration in PHP examples

---
+++ learn-php/PHPCrashCourse/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "We talk about variables".'<br>';
+print("Hello world!<br>");
+print_r(['name' => 'Hoang','age' => 18, 'has_mercedes' => true, 'product_price' => 22.45]);
+?>
--- learn-php/PHPCrashCourse/variables.php
+++ learn-php/PHPCrashCourse/variables.php
@@ -1,26 +1,38 @@
 <?php
-    // echo "We talk about variables".'<br>';
+    echo "We talk about variables".'<br>';
     $name = 'Hoang'; //string
     $age = 18;//integer
-    // echo $age;
+    echo $age;
     $has_mercedes = false;
-    //echo $has_mercedes;
-    //var_dump($has_mercedes);
+    echo $has_mercedes;
+    var_dump($has_mercedes);
     $product_price = 22.45; //Float
-    //var_dump($product_price);
+    var_dump($product_price);
     echo "<br>";
     //string concatenation
-   //echo $name.' is '.$age.' years old'."<br>";
+   echo $name.' is '.$age.' years old'."<br>";
    //this is better
-   //echo "${name} is ${age} years old...";
+   echo "$name is $age years old...";
+    echo "<br>";
+
    //expression
-   //echo '1' + '2';
+   echo '1' + '2';
+    echo "<br>";
+
    $sum = '2' + '3';
-   //var_dump($sum);
-   //echo 5 * 3;
-   //echo 10 / 2;
-   //echo 5 / 0;
+   echo "<br>";
+   var_dump($sum);
+
+   echo 5 * 3;
+    echo "<br>";
+
+   echo 10 / 2;
+    echo "<br>";
+
+//    echo 5 / 0;
     echo 10 % 3;//remains
+    echo "<br>";
+
     //constants
     define('SERVER_NAME', 'localhost');
     define('DATABASE_NAME', 'test_db');
