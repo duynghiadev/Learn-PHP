9656edd0dee080ad183efc139fe850e177a4dd5c
duynghiadev <duynghia22302@gmail.com>
2025-06-12 17:33:22 +0700

    fix

---
+++ project-php/php-oop/index.php
@@ -0,0 +1,19 @@
+<?php
+// index.php - Entry point for OOP PHP examples
+
+echo "<h1>PHP OOP Examples Index</h1>";
+echo "<ul>";
+echo "<li><a href='./class.php'>Class Example</a></li>";
+echo "<li><a href='./chaining-with-this.php'>Chaining with \$this Example</a></li>";
+echo "<li><a href='./access-private-props.php'>Access Private Properties Example</a></li>";
+echo "<li><a href='./inheritance.php'>Inheritance Example</a></li>";
+echo "<li><a href='./abstract-classess.php'>Abstract Classes Example</a></li>";
+echo "<li><a href='./interface.php'>Interface Example</a></li>";
+echo "<li><a href='./polymorphism.php'>Polymorphism Example</a></li>";
+echo "<li><a href='./traits.php'>Traits Example</a></li>";
+echo "<li><a href='./dependency-injection.php'>Dependency Injection Example</a></li>";
+echo "<li><a href='./exception-handling.php'>Exception Handling Example</a></li>";
+echo "</ul>";
+
+echo "<p>See <a href='../readme.md'>readme.md</a> for documentation and notes.</p>";
+?>
