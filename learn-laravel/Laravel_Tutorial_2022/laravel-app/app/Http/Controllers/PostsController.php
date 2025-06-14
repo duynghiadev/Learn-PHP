7e349b693e5cfee252c8159efa1ab78ae3272567
duynghiadev <duynghia22302@gmail.com>
2025-06-14 10:52:34 +0700

    style(controllers): improve code formatting and readability

--- learn-laravel/Laravel_Tutorial_2022/laravel-app/app/Http/Controllers/PostsController.php
+++ learn-laravel/Laravel_Tutorial_2022/laravel-app/app/Http/Controllers/PostsController.php
@@ -7,40 +7,41 @@ use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         //Query builders
         // $posts = DB::select("SELECT * FROM posts WHERE id = :id;",
         // [
         //     'id' => 3
         // ]);
         $posts = DB::table("posts")
-                    ->where('id', '=', 5)
-                    ->delete();
-                    // ->update([
-                    //     'title' => 'haha title',
-                    //     'body' => 'This is haha body',
-                    // ]);
+            ->where('id', '=', 5)
+            ->delete();
+        // ->update([
+        //     'title' => 'haha title',
+        //     'body' => 'This is haha body',
+        // ]);
 
-                    // ->insert([
-                    //     'title' => 'haha',
-                    //     'body' => 'A new post hahaha'
-                    // ]);
+        // ->insert([
+        //     'title' => 'haha',
+        //     'body' => 'A new post hahaha'
+        // ]);
 
-                    //->avg('id');//average
-                    //->sum('id');
-                    //->min('id');
-                    //->count();
-                    //->find(3);//find by id
-                    //->whereNotNull("body")
-                    //->oldest()
-                    //->latest()
-                    //->orderBy('id', 'asc')
-                    // ->whereBetween("id", [1, 3])
-                    // ->where("created_at",">", now()->subDay())
-                    // ->orWhere('id', '>', 0)
-                    //->select('body')
-                    //->first();
-                    //->get();
+        //->avg('id');//average
+        //->sum('id');
+        //->min('id');
+        //->count();
+        //->find(3);//find by id
+        //->whereNotNull("body")
+        //->oldest()
+        //->latest()
+        //->orderBy('id', 'asc')
+        // ->whereBetween("id", [1, 3])
+        // ->where("created_at",">", now()->subDay())
+        // ->orWhere('id', '>', 0)
+        //->select('body')
+        //->first();
+        //->get();
 
         dd($posts); //dd = die dump
         //return view('posts.index');
